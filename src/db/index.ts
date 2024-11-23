9ab23a8896b9f8b55d20114331570ab9a3b452f1
Heisenberg <jon@jonrose.com>
2024-11-22 22:54:04 -0800

    connects to prod

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,14 @@
 import postgres from 'postgres';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import * as schema from './schema';
+import { readFileSync } from 'fs';
 
 // Create the postgres connection
-export const connection = postgres(process.env.POSTGRES_URL!);
+export const connection = postgres(process.env.POSTGRES_URL!, {
+  ssl: {
+    ca: readFileSync('./certs/prod-ca-2021.crt').toString()
+  }
+});
 
 // Create the database instance
 export const db = drizzle(connection, { schema });
